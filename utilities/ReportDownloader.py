4de5ad5a733907af23c909414a348d940cf2ef51
RaghuPythonProjects <118695122+RaghuPythonProjects@users.noreply.github.com>
2024-10-02 22:17:24 +0530

    handle download large data

--- utilities/ReportDownloader.py
+++ utilities/ReportDownloader.py
@@ -15,20 +15,28 @@ class ReportDownloader:
     def download_report_from_api(self, report_id, target_filename):
         """
         Download the latest report for the given report ID and save it to the specified target filename.
+        Uses streaming to handle large files.
         """
         try:
             url = f"https://vmo7222pa005.otis.com:3780/api/3/reports/{report_id}/history/latest/output"
-            response = requests.request("GET", url, headers=self.report_api_headers, verify=False)
-            logger.info(
-                f"Downloading report {report_id}, Response Status: {response.status_code}, "
-                f"content length: {len(str(response.content))}, content: {str(response.content)[:250]}"
-            )
+            
+            # Use streaming for downloading large files
+            with requests.get(url, headers=self.report_api_headers, verify=False, stream=True) as response:
+                response.raise_for_status()  # Raise an error if the request failed
+                total_size = response.headers.get('content-length')
 
-            # Save the content to the file
-            with open(target_filename, "wb") as file:
-                file.write(response.content)
+                logger.info(
+                    f"Downloading report {report_id}, Response Status: {response.status_code}, "
+                    f"content length: {total_size if total_size else 'unknown'}"
+                )
 
-            logger.info(f"Report {report_id} saved to {target_filename}")
+                # Save the content to the file in chunks
+                with open(target_filename, "wb") as file:
+                    for chunk in response.iter_content(chunk_size=1024 * 1024):  # 1 MB chunks
+                        if chunk:  # Filter out keep-alive new chunks
+                            file.write(chunk)
+
+            logger.info(f"Report {report_id} successfully saved to {target_filename}")
             return True
 
         except Exception as e:
